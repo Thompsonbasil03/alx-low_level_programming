df91fe49f34340e75b28937b083e8f33a4fcd28e
Thompsonbasil03 <basilthompson98@gmail.com>
2022-10-20 02:40:25 -0700

    Tasak 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,21 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
+* @i: test that the function positive_or_negative() gives
+* the correct output when give* n a case of 0
+*
 * Return: 0
 */
 
-int main(void)
+int main(int i)
 {
 int i;
 
 i = 98;
 
-printf("positive_or_negative:('\n'};
+printf("positive_or_negative i:('\n'};
+
+else
 
 return (0);
 }
